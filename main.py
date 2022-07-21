bebabbd90cd464edc9e6df93795c0f7def249287
yongyonghae <109565224+yongyonghae@users.noreply.github.com>
2022-07-21 10:23:04 +0900

    ChampName - 아리 추가

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 
 url = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Aatrox.json'
+url2 = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Ahri.json'
+
 res = requests.get(url).json()
+res2 = requests.get(url2).json()
 
 champ_lst = (res['data']['key'], res['data']['Aatrox']['name'])
+champ_lst2 = (res2['data']['key'], res2['data']['Ahri']['name'])
